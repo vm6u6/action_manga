47b1501e24def0ce2eb205ccbf0ed1c2e0be0652
vm6u6 <ih.hsu1234@gmail.com>
2025-03-27 01:06:36 +0800

    fix

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ from utils import convert_to_jpg
 import cv2
 from translate import M2M_translate, LLM_translate
 from inpaint_img import infer_inpaint
-from pannel_dectect import pannel_infer
+from pannel_detect import pannel_infer
 
 class action_manga():
     def __init__(self, folder_path):
@@ -81,44 +81,44 @@ class action_manga():
         image_path, image_name = self.load_data_path()
         print("[INFO] Dealing with images: ", image_name)
 
-        # ### { Crop image OCR / Inpainting } ###
-        # cnt = 0
-        # image_xyxy_list = []
-        # for img_path in image_path:
-        #     save_path = img_path.split(".")[0] + ".jpg"
-        #     convert_to_jpg(img_path, save_path)
+        ### { Crop image OCR / Inpainting } ###
+        cnt = 0
+        image_xyxy_list = []
+        for img_path in image_path:
+            save_path = img_path.split(".")[0] + ".jpg"
+            convert_to_jpg(img_path, save_path)
 
-        #     if save_path.endswith("jpg"):
-        #         image = cv2.imread(save_path)
-        #         # croppedImageData = findSpeechBubbles(img_path, "complex", self.crop_textPath)                     # OpenCV Version (Not recommend)
-        #         crpppedImageData, image_xyxy = self.seg_text.run(save_path)                                         # Roboflow
-        #         self.save_cropped_img(image_xyxy, image, image_name[cnt])
-        #         image_xyxy_list.append(image_xyxy)
+            if save_path.endswith("jpg"):
+                image = cv2.imread(save_path)
+                # croppedImageData = findSpeechBubbles(img_path, "complex", self.crop_textPath)                     # OpenCV Version (Not recommend)
+                crpppedImageData, image_xyxy = self.seg_text.run(save_path)                                         # Roboflow
+                self.save_cropped_img(image_xyxy, image, image_name[cnt])
+                image_xyxy_list.append(image_xyxy)
 
-        #         inpainted_image = self.inpaint_engine.run(image, image_xyxy, margin=5)
-        #         inpainted_save_path = self.inpaint_img_path + image_name[cnt] + "_inpainted.jpg"
-        #         cv2.imwrite(inpainted_save_path, cv2.cvtColor(inpainted_image, cv2.COLOR_RGB2BGR))
-        #         print(f"Save inpaint img to : {inpainted_save_path}")
+                inpainted_image = self.inpaint_engine.run(image, image_xyxy, margin=5)
+                inpainted_save_path = self.inpaint_img_path + image_name[cnt] + "_inpainted.jpg"
+                cv2.imwrite(inpainted_save_path, cv2.cvtColor(inpainted_image, cv2.COLOR_RGB2BGR))
+                print(f"Save inpaint img to : {inpainted_save_path}")
 
-        #         cnt += 1
-        # croppedImageList = os.listdir(self.crop_textPath)
+                cnt += 1
+        croppedImageList = os.listdir(self.crop_textPath)
 
-        # ### { Translate } ###
-        # img_text = []
-        # for crop_img in croppedImageList:   
-        #     text_res = self.ocr_engine.text_generator(self.crop_textPath + "/" + crop_img)
-        #     # translated_text = self.translate_engine.run(text_res, self.src_lang, self.target_lang)                # m2m
-        #     # translated_text = self.translate_engine.translate_text(text_res)                                      # Gemini 有次數限制
-        #     img_text.append(text_res)
+        ### { Translate } ###
+        img_text = []
+        for crop_img in croppedImageList:   
+            text_res = self.ocr_engine.text_generator(self.crop_textPath + "/" + crop_img)
+            # translated_text = self.translate_engine.run(text_res, self.src_lang, self.target_lang)                # m2m
+            # translated_text = self.translate_engine.translate_text(text_res)                                      # Gemini 有次數限制
+            img_text.append(text_res)
 
-        #     ### [TODO] length limited with gemini
+            ### [TODO] length limited with gemini
 
-        # translated_text_list = self.translate_engine.translate_txt_list(img_text)
-        # for i in range( len(croppedImageList) ):  
-        #     crop_img = croppedImageList[i]
-        #     translated_text = translated_text_list[i]
-        #     with open(self.save_translate_txt_path+ crop_img.split(".jpg")[0] + ".txt", 'w', encoding='utf-8') as f:
-        #         f.write(translated_text)
+        translated_text_list = self.translate_engine.translate_txt_list(img_text)
+        for i in range( len(croppedImageList) ):  
+            crop_img = croppedImageList[i]
+            translated_text = translated_text_list[i]
+            with open(self.save_translate_txt_path+ crop_img.split(".jpg")[0] + ".txt", 'w', encoding='utf-8') as f:
+                f.write(translated_text)
 
 
         ### { Animate } ###
